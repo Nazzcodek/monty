5c9edffd7c4e375a7570f512a29de706d83ea558
Nazzcodek <bellnas09@gmail.com>
2023-06-21 11:08:04 -0700

    solved

---
+++ pall.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pall - pall function
+ *
+ * @stack: pointer to stack pointer
+ *
+ * @line_number: line number
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "Error: L%d: Stack not initialized\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
---
+++ push.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+/**
+ * push - the push function
+ *
+ * @stack: pointer to stack pointer
+ * @line_number: line Number
+ *
+ * Return: new node from stack
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	char *arg = strtok(NULL, " \n");
+	int value = atoi(arg);
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "Error: L%d: Stack not initialized\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: L%d: malloc failed\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!arg || !(*arg))
+	{
+		fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+
+	if (value == 0 && *arg != '0')
+	{
+		fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
