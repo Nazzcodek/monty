8f9a570d39b6eb25bddfe44c7a3eb9d45f3cbf34
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2023-06-21 16:17:13 +0100

    Update push.c

--- push.c
+++ push.c
@@ -11,26 +11,43 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *top = NULL, *new_node;
+	stack_t *new_node;
+	char *arg = strtok(NULL, " \n");
+	int value = atoi(arg);
 
-	(void)line_number;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	if (!(*stack))
 	{
-		fprintf(stderr, "Error: malloc failed");
+		fprintf(stderr, "Error: L%d: Stack not initialized\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = data_;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	if (*stack == NULL)
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
 	{
-		*stack = new_node;
-		return;
+		fprintf(stderr, "Error: L%d: malloc failed\n", line_number);
+		exit(EXIT_FAILURE);
 	}
-	top = *stack;
-	top->prev = new_node;
-	new_node->next = top;
+
+	if (!arg || !(*arg))
+	{
+		fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+
+	if (value == 0 && *arg != '0')
+	{
+		fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
 	*stack = new_node;
-	return;
 }
